e3cc64c5a14503ebae905cd695c30a9cc7d13b35
Ethan Glasser-Camp <ethan@betacantrips.com>
2015-12-08 18:50:16 -0500

    initial implementation

---
+++ README.md
@@ -0,0 +1,4 @@
+# canteven-listen-http: help configure a web service
+
+This library is meant to help parse a config file for a web service
+and figure out on what ports to serve.
---
+++ src/Canteven/Listen/HTTP.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
+-- | Types for the canteven-serve-with-ekg library.
+module Canteven.Listen.HTTP (
+    ListenerConfig(..),
+    Scheme(..),
+    WithListenHTTPConfig(..),
+    ) where
+
+import Data.Aeson (FromJSON(parseJSON), withObject, (.:))
+import GHC.Generics (Generic)
+
+-- | A type representing the ports to listen
+data WithListenHTTPConfig userConfig =
+    WithListenHTTPConfig {
+        listeners :: [ListenerConfig],
+        userConfig :: userConfig
+    } deriving (Generic)
+
+instance (FromJSON c) => FromJSON (WithListenHTTPConfig c) where
+    parseJSON v = flip (withObject "WithListenHTTPConfig") v $ \obj -> do
+        listeners <- obj .: "listen"
+        userConfig <- parseJSON v
+        return WithListenHTTPConfig {
+            listeners,
+            userConfig
+        }
+
+-- | A type representing a single "listener".
+--
+-- An application can listen to many of these
+data ListenerConfig =
+    ListenerConfig {
+        scheme :: Scheme,
+        port :: Int,
+        cert :: Maybe FilePath,
+        key :: Maybe FilePath
+    } deriving (Generic)
+
+instance FromJSON ListenerConfig
+
+
+data Scheme = HTTP | HTTPS
+    deriving (Generic, Eq, Show)
+
+instance FromJSON Scheme
